ee1b7cc65e5f7454fa6323a77d5c8ab89f212310
RBElano <rbenjaminelano@gmail.com>
2025-12-02 23:19:22 +0800

    Changed baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const api = axios.create({
-baseURL: 'http://localhost:4000'
+baseURL: 'https://fitnessapp-api-ln8u.onrender.com'
 })
 
 api.interceptors.request.use((config) => {
